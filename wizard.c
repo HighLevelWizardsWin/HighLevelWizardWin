101b393a43f9a1f54cad869f45885d2f8b2fd33e
Isaac Evans <Nocluedrummer37@yahoo.com>
2015-10-29 15:07:49 -0400

    Changed logic regarding wizard status

--- wizard.c
+++ wizard.c
@@ -30,82 +30,85 @@ void *wizard_func(void *wizard_descr)
   /* Infinite loop */
   while (1)
   {
-    /* Loops until he's able to get a hold on both the old and new rooms */
-    while (1)
-	{
-	  printf("Wizard %c%d in room (%d,%d) wants to go to room (%d,%d)\n",
-	  self->team, self->id, oldroom->x, oldroom->y, newroom->x, newroom->y);
-	  
-	  if (!try_room(self, oldroom, newroom))
+  	if (self->status == 0) // Needed? Could be controlled somewhere else
+  	{
+      /* Loops until he's able to get a hold on both the old and new rooms */
+      while (1)
 	  {
-        /* Waits a random amount of time */
-        printf("Too bad! The door is locked. You should learn teleport.");
-        dostuff();
-        
-        /* Chooses the new room */
-        newroom = choose_room(self);
-        
-        /* Goes back to the initial state and try again */
-        continue;
-      }
-	  else break;
-	}
-      
-    printf("Wizard %c%d in room (%d,%d) moves to room (%d,%d)\n",
-	   self->team, self->id, 
-	   oldroom->x, oldroom->y, newroom->x, newroom->y);
-
-    /* Fill in */
-    
-    /* Self is active and has control over both rooms */
-    switch_rooms(self, oldroom, newroom);
-
-    other = find_opponent(self, newroom);
-
-    /* If there is not another wizard does nothing */
-    if (other == NULL)
-	{
-	  printf("Wizard %c%d in room (%d,%d) finds nobody around \n",
-		 self->team, self->id, newroom->x, newroom->y);
-	  /* Fill in */
-	}
-    else
-	{
-	  /* Other is from opposite team */
-	  if (other->team != self->team)
-	  {
-	    /* Checks if the opponent is active */
-	    if (other->status == 0)
+	    printf("Wizard %c%d in room (%d,%d) wants to go to room (%d,%d)\n",
+	    self->team, self->id, oldroom->x, oldroom->y, newroom->x, newroom->y);
+	    
+	    if (!try_room(self, oldroom, newroom))
 	    {
-		  printf("Wizard %c%d in room (%d,%d) finds active enemy\n",
-		  self->team, self->id, newroom->x, newroom->y);
-
-		  fight_wizard(self, other, newroom);
-		}
-	    else
-		{
-		  printf("Wizard %c%d in room (%d,%d) finds enemy already frozen\n",
-			 self->team, self->id, newroom->x, newroom->y);
-		}
+          /* Waits a random amount of time */
+          printf("Too bad! The door is locked. You should learn teleport.");
+          dostuff();
+          
+          /* Chooses the new room */
+          newroom = choose_room(self);
+          
+          /* Goes back to the initial state and try again */
+          continue;
+        }
+	    else break;
 	  }
-	  else /* Other is from same team */
-	  {
-	    /* Checks if the friend is frozen */
-	    if (other->status == 1)
-		{
-		  free_wizard(self, other, newroom);
-		}
-	  }
-
-	  /* Fill in */
-
-	}
-
-    /* Thinks about what to do next */
-    dostuff();
-
-    oldroom = newroom;
-    newroom = choose_room(self);
+        
+      printf("Wizard %c%d in room (%d,%d) moves to room (%d,%d)\n",
+	     self->team, self->id, 
+	     oldroom->x, oldroom->y, newroom->x, newroom->y);
+  
+      /* Fill in */
+        
+      /* Self is active and has control over both rooms */
+      switch_rooms(self, oldroom, newroom);
+  
+      other = find_opponent(self, newroom);
+  
+      /* If there is not another wizard does nothing */
+      if (other == NULL)
+  	  {
+  	    printf("Wizard %c%d in room (%d,%d) finds nobody around:  ¯\_(ツ)_/¯  \n",
+  	  	 self->team, self->id, newroom->x, newroom->y);
+  	    /* Fill in */
+  	  }
+      else
+  	  {
+  	    /* Other is from opposite team */
+  	    if (other->team != self->team)
+  	    {
+  	      /* Checks if the opponent is active */
+  	      if (other->status == 0)
+  	      {
+  	  	  printf("Wizard %c%d in room (%d,%d) finds active enemy\n",
+  	  	  self->team, self->id, newroom->x, newroom->y);
+  
+  	  	  fight_wizard(self, other, newroom);
+  	  	  }
+  	      else
+  	  	  {
+  	  	    printf("Wizard %c%d in room (%d,%d) finds enemy already frozen\n",
+  	  		 self->team, self->id, newroom->x, newroom->y);
+  	  	  }
+  	    }
+  	    else /* Other is from same team */
+  	    {
+  	      /* Checks if the friend is frozen */
+  	      if (other->status == 1)
+  	  	  {
+  	  	    free_wizard(self, other, newroom);
+  	  	  }
+  	    }
+  
+  	    /* Fill in */
+  
+  	  }
+  
+      /* Thinks about what to do next */
+      dostuff();
+  
+      oldroom = newroom;
+      newroom = choose_room(self);
+    }
   }
   
   return NULL;
